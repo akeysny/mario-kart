efcbcdbcfcdb603a5481cab1b81c6603d536259e
Anna Taylor <annatnyc@gmail.com>
2021-07-17 16:39:33 -0400

    Initial Commit

---
+++ mario-kart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mario-kart
+//
+//  Created by Anna Taylor on 7/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
